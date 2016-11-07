47e575478ce6f02b76c6f027ebf8d2a63de71f8b
volodymyrmigdal <volodymyr.migdal@gmail.com>
2016-11-07 18:36:07 +0200

    experiment update

--- sample/Experiment.js
+++ sample/Experiment.js
@@ -9,10 +9,11 @@ var _ = wTools;
 
 var logger = new wLogger();
 
-logger.log( 'no color text',_.strColor.fg( 'red text','red' ),_.strColor.bg( 'yellow background','yellow' ) );
-
-logger.log( '#foreground : red#this is red text' );
-logger.log( 'this is too#foreground : default#' );
-logger.log( '#background : green#green background' );
-logger.log( 'this is too#background : default#' );
-logger.log( 'no color text' );
+// logger.log( 'no color text',_.strColor.fg( 'red text','red' ),_.strColor.bg( 'yellow background','yellow' ) );
+//
+// logger.log( '#foreground : red#this is red text' );
+// logger.log( 'this is too#foreground : default#' );
+// logger.log( '#background : green#green background' );
+// logger.log( 'this is too#background : default#' );
+// logger.log( 'no color text' );
+logger.log( _.strColor.bg('yellow bg'+ _.strColor.bg( 'green bg' ,'green') + 'yellow bg', 'yellow' ) );
