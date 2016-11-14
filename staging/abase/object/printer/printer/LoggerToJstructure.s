bdc09bf759afe798051d868dfc5fad2748522833
volodymyrmigdal <volodymyr.migdal@gmail.com>
2016-11-14 13:07:25 +0200

    LoggerToJstructure update

--- staging/abase/object/printer/printer/LoggerToJstructure.s
+++ staging/abase/object/printer/printer/LoggerToJstructure.s
@@ -14,6 +14,7 @@ if( typeof module !== 'undefined' )
 
 //
 
+
 var _ = wTools;
 var Parent = wLogger;
 var Self = function wLoggerToJstructure()
@@ -36,7 +37,65 @@ var init = function( o )
 
   if( self.output )
   self.outputTo( self.output );
+}
 
+var init_static = function()
+{
+  var proto = this;
+  _.assert( Object.hasOwnProperty.call( proto,'constructor' ) );
+
+  for( var m = 0 ; m < proto.outputWriteMethods.length ; m++ )
+  proto._init_static( proto.outputWriteMethods[ m ] );
+
+}
+
+//
+
+var _init_static = function( name )
+{
+  var proto = this;
+
+  _.assert( Object.hasOwnProperty.call( proto,'constructor' ) )
+  _.assert( arguments.length === 1 );
+  _.assert( _.strIs( name ) );
+
+  var nameAct = name + 'Act';
+
+  /* */
+
+  var write = function()
+  {
+    return this._writeToStruct.apply(this, arguments );
+  }
+
+  proto[ name ] = write;
+}
+
+//
+
+var _writeToStruct = function()
+{
+  if( !arguments.length )
+  return;
+
+  var data = _.strConcat.apply( {},arguments );
+  var _changeLevel = function( arr, level )
+  {
+    if( !level )
+    return arr;
+    if( !arr[ 0 ] )
+    arr[ 0 ] = [ ];
+    return _changeLevel( arr[ 0 ], --level );
+  }
+  _changeLevel( this.structure, this.level ).push( data );
+}
+
+//
+
+var toJson = function()
+{
+  var self = this;
+  return _.toStr( self.structure, { json : 1 } );
 }
 
 // --
@@ -53,8 +112,9 @@ var Aggregates =
 
 var Associates =
 {
-  output : null,
+  output : console,
   outputData : [],
+  structure : [],
 }
 
 // --
@@ -65,6 +125,12 @@ var Proto =
 {
 
   init : init,
+  init_static : init_static,
+  _init_static : _init_static,
+
+  _writeToStruct : _writeToStruct,
+
+  toJson : toJson,
 
   // relationships
 
@@ -83,6 +149,8 @@ _.protoMake
   parent : Parent,
   extend : Proto,
 });
+
+Self.prototype.init_static();
 
 // --
 // export
