77cb84373167c6af71ff5dfbc58706b0b55e40d3
vmigdal <volodymyr.migdal@gmail.com>
2018-07-26 13:11:22 +0300

    adjusted samples2/OriginalOutputToLogger.s

--- sample2/OriginalOutputToLogger.s
+++ sample2/OriginalOutputToLogger.s
@@ -11,7 +11,13 @@ _.include( 'wLogger' );
 console.log( 'Please see ExclusiveOutputFromConsole.s first' );
 console.log( 'Setup exclusive input from logger l1 -> l2' );
 
-let l1 = new _.Logger({ name : 'l1' });
+let hooked = '';
+let l1 = new _.Logger
+({
+    name : 'l1',
+    onTransformBegin : onTransformBegin,
+    onTransformEnd : onTransformEnd
+});
 let l2 = new _.Logger({ name : 'l2' });
 
 l1.outputTo( console );
@@ -23,8 +29,23 @@ let l3 = new _.Logger({ name : 'l3' });
 // l3.outputTo( l1, { originalOutput : 1 } );
 l1.inputFrom( l3, { originalOutput : 1 } );
 
-l1.log( 'This message will neither get on screen' );
-l3.log( 'This message will get on screen' );
+l1.log( 'This message will neither get on screen, because l1 -> exclusiveOutput -> l2' );
+l3.log( 'This message will not get on screen, because l3 -> originalOutput -> l1, link is terminating' );
+
+function onTransformBegin( o )
+{
+    hooked += '( transformOnBegin ) ' + this.name + ' : ' + o.input[ 0 ] + '\n';
+    return o;
+}
+
+function onTransformEnd( o )
+{
+    hooked += '( transformOnEnd ) ' + this.name + ' : ' + o.input[ 0 ] + '\n';
+    return o;
+}
+
+console.log( 'Link l3 -> originalOutput -> l1 is terminating, but message reaches onTransform* handlers anyway' );
+console.log( '```\n' + hooked + '```' );
 
 /*
     Chain:
